006c5436952791817583a64e6166233025d26bb9
sdw <sdw@inea.com>
2018-08-02 15:14:27 -0400

    Initial commit

--- DynamicAnimationTut/Properties/AssemblyInfo.cs
+++ DynamicAnimationTut/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using Android.App;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DynamicAnimationTut")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
